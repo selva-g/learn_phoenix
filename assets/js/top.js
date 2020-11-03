12681976974e0e265cb87d34d512f9a8e265ce6d
selva-g <selvagvinayak@gmail.com>
2020-11-03 14:06:45 +0530

    V3
    Added the seearch engine icon

--- assets/js/top.js
+++ assets/js/top.js
@@ -55,3 +55,20 @@ function myFunction() {
   }
   
 //end of b toggle
+
+//$('.search-button').click(function(){
+//	$(this).parent().toggleClass('open');
+ // });
+
+
+  $(document).ready(function(){
+
+	$("#inpt_search").on('focus', function () {
+		$(this).parent('label').addClass('active');
+	});
+	
+	$("#inpt_search").on('blur', function () {
+		if($(this).val().length == 0)
+			$(this).parent('label').removeClass('active');
+	});
+	});
